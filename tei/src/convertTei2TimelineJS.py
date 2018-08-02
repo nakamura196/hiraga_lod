d61bb62b474b2c82527de1520aba5912181430cb
satoru196 <na.kamura.1263@gmail.com>
2018-08-02 22:25:33 +0900

    minor update.

--- tei/src/convertTei2TimelineJS.py
+++ tei/src/convertTei2TimelineJS.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import argparse
 import sys
+import myfunc as mf
 
 prefix = ".//{http://www.tei-c.org/ns/1.0}"
 prefix2 = "{http://www.tei-c.org/ns/1.0}"
@@ -23,94 +24,6 @@ def parse_args(args=sys.argv[1:]):
     return parser.parse_args(args)
 
 
-def getFacs(root):
-    facs_dict = {}
-
-    surfaces = root.findall(prefix + "surface")
-    for surface in surfaces:
-        url = surface.find(prefix + "graphic").get("url")
-
-        zones = surface.findall(prefix + "zone")
-        for zone in zones:
-            id = zone.get(prefix3 + "id")
-            lry = int(zone.get("lry"))
-            lrx = int(zone.get("lrx"))
-            uly = int(zone.get("uly"))
-            ulx = int(zone.get("ulx"))
-
-            x = ulx
-            y = uly
-            dx = lrx - ulx
-            dy = lry - uly
-
-            area = str(x) + "," + str(y) + "," + str(dx) + "," + str(dy)
-
-            target_url = url.replace("full/full", area + "/full")
-
-            facs_dict[id] = target_url
-
-    return facs_dict
-
-
-def get_thumbnail_from_dbpedia(dbpedila_url):
-    thumbnail = ""
-    return thumbnail
-
-
-def get_people(root):
-    people_dict = {}
-
-    people = root.findall(prefix + "person")
-    for person in people:
-
-        id = person.get(prefix3 + "id")
-
-        # thumbnail = ""
-        dbpedia_url = ""
-        if person.find(prefix + "persName").get("ref") != None:
-            dbpedia_url = person.find(prefix + "persName").get("ref")
-
-            # thumbnail = get_thumbnail_from_dbpedia(dbpedia_url)
-
-        occupation_list = []
-        occupations = person.findall(prefix + "occupation")
-        for occupation in occupations:
-            occupation_list.append(occupation.text)
-
-        occupation_str = ""
-        if len(occupation_list) > 0:
-            occupation_str = "〔"
-            for i in range(len(occupation_list)):
-                occupation_str += occupation_list[i]
-                if i != len(occupation_list) - 1:
-                    occupation_str += ", "
-            occupation_str += "〕"
-
-        person_dict = {}
-        people_dict[id] = person_dict
-        person_dict["persName"] = id
-        person_dict["dbpedia_url"] = dbpedia_url
-        person_dict["occupation"] = occupation_str
-
-    return people_dict
-
-
-def get_org(root):
-    org_dict = {}
-
-    orgs = root.findall(prefix + "org")
-    for org in orgs:
-
-        id = org.get(prefix3 + "id")
-        note = ""
-        if org.find(prefix + "note") != None:
-            note = " 〔" + org.find(prefix + "note").text + "〕"
-
-        org_dict[id] = id + note
-
-    return org_dict
-
-
 def main(path_to_xml):
     inputpath = path_to_xml
     outputpath = inputpath + ".csv"
@@ -124,9 +37,9 @@ def main(path_to_xml):
 
     # 改行コード（\n）を指定しておく
 
-    facs_dict = getFacs(root)
-    people_dict = get_people(root)
-    org_dict = get_org(root)
+    facs_dict = mf.getFacs(root)
+    people_dict = mf.get_people(root)
+    org_dict = mf.get_org(root)
     print(org_dict)
     print(people_dict)
 
